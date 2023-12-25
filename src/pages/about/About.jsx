37761bdf7f36059b417012194898542531100ed6
Rokkam Nikhila <115566678+nikhilarokkam@users.noreply.github.com>
2023-12-25 08:22:27 +0530

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Info from '../../components/Info';
 import Stats from '../../components/Stats';
 import {FaDownload} from "react-icons/fa";
-import CV from "../../assets/Rokkam_Nikhila_Resume.pdf";
+import CV from "../../assets/Rokkam-Nikhila-Resume.pdf";
 import Skills from '../../components/Skills';
 import { resume } from '../../data';
 import ResumeItem from '../../components/ResumeItem';
