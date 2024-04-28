3a3b4f7444b80eed6a1a6df76707bd3c6045bda2
Rokkam Nikhila <115566678+nikhilarokkam@users.noreply.github.com>
2024-04-28 12:24:10 +0530

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Info from '../../components/Info';
 import Stats from '../../components/Stats';
 import {FaDownload} from "react-icons/fa";
-import CV from "../../assets/Resume1.pdf";
+import CV from "../../assets/Rokkam Nikhila Resume 1.pdf";
 import Skills from '../../components/Skills';
 import { resume } from '../../data';
 import ResumeItem from '../../components/ResumeItem';
