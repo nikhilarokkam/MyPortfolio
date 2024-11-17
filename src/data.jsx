b12b6926af59c146a58d5a8b7679cf6c80ea3744
Rokkam Nikhila <115566678+nikhilarokkam@users.noreply.github.com>
2024-11-17 13:15:05 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -11,7 +11,7 @@ import {
 import { FiFileText, FiUser, FiExternalLink } from 'react-icons/fi';
 
 import Work1 from './assets/Artboard-1.png';
-import Work2 from './assets/project-2.jpg';
+import Work2 from './assets/cropped-cropped-birthday-1.png';
 import Work3 from './assets/project-3.jpeg';
 import Work4 from './assets/project-4.jpeg';
 import Work5 from './assets/project-5.jpg';
